f0cf3adaedbf3f549b9fc29cbe747247782deb6a
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2017-12-06 16:35:39 -0800

    Display latest print issue on home page

--- ubyssey/views/main.py
+++ ubyssey/views/main.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import json
+import requests
+from bs4 import BeautifulSoup
 
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, Http404
@@ -54,6 +56,14 @@ class UbysseyTheme(object):
 
         title = '%s - UBC\'s official student newspaper' % self.SITE_TITLE
 
+        # Scraping the URL and cover image URL of Ubyssey's latest print issue
+        page = requests.get('http://search.issuu.com/ubyssey/docs/recent.rss')
+        soup = BeautifulSoup(page.content, 'xml')
+        latestIssue = soup.findAll('item')[0]
+
+        issuu_url = latestIssue.find('link').get_text()
+        issuu_img = latestIssue.find('media:content').get('url')
+
         context = {
             'title': title,
             'meta': {
@@ -67,6 +77,8 @@ class UbysseyTheme(object):
             'popular': popular,
             'blog': blog,
             'day_of_week': datetime.now().weekday(),
+            'issuu_url': issuu_url,
+            'issuu_img': issuu_img
         }
         return render(request, 'homepage/base.html', context)
 
