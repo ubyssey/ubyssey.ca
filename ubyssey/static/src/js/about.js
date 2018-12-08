1b85d0b328789afb6eeb9b6ed194d44b32b1b480
ogadoury <32474674+ogadoury@users.noreply.github.com>
2018-12-08 11:57:43 -0800

    Changes for production

--- ubyssey/static/src/js/about.js
+++ ubyssey/static/src/js/about.js
@@ -1,32 +1,41 @@
 // this code triggers animations when the elements are scrolled into view
 
 
-	var animateHTML = function() {
-	  var windowHeight;
-	  function init() {
-	  	//slide right and slide left elements elements
-	    var elems = $('.slide-right') + $('.slide-left');
+  var animateHTML = function() {
+    var elemsL;
+    var elemsR;
+    var windowHeight;
+    function init() {
+      //slide right and slide left elements elements
+      elemsL = document.querySelectorAll('.slide-left');
+      elemsR = document.querySelectorAll('.slide-right');
 
-	    windowHeight = $(window).innerHeight;
-	    addEventHandlers();
-	    checkPosition();
-	  }
-	  function addEventHandlers() {
-	  	$(window).scroll(checkPosition);
-	  	$(window).resize(init);
-	    // window.addEventListener('scroll', checkPosition);
-	    // window.addEventListener('resize', init);
-	  }
-	  function checkPosition() {
-	    for (var i = 0; i < elems.length; i++) {
-	      var positionFromTop = elems[i].getBoundingClientRect().top;
-	      if (positionFromTop - windowHeight <= 0) {
-	        elems[i].addClass('slide-anim');
-	      }
-	    }
-	  }
-	  return {
-	    init: init
-	  };
-	};
-	animateHTML().init();
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      // function to check the position of selected elements
+      for (var i = 0; i < elemsL.length; i++) {
+        var positionFromTop = elemsL[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          elemsL[i].classList.add('slide-anim');
+        }
+      }
+      for (var j = 0; j < elemsR.length; j++) {
+        var positionFromTop = elemsR[j].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= -100) {
+          elemsR[j].classList.add('slide-anim');
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+
+  animateHTML().init();
