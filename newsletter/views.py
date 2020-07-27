b5e56386fef39e80c1fd0250f2d944f06c0fb317
Keegan Landrigan <keeganland@gmail.com>
2020-07-27 21:43:40 +0000

    fill out boilerplate code from Django site

--- newsletter/views.py
+++ newsletter/views.py
@@ -4,18 +4,20 @@ from .forms import SubscriberForm
 
 # Create your views here.
 
-
-#TODO
-
 class SubscriberFormView(View):
     form_class = SubscriberForm
-    template_name = 'something.html'
+    
+    # Template that will embed the form
+    template_name = 'newsletter_form.html'
 
     #Need to link this to the frontend stuff David made, somehow
     def get(self, request, *args, **kwargs):
-        #form = self.form_class
+        form = self.form_class
         return render(request, self.template_name)
     def post(self, request, *args, **kwards):
         form = self.form_class(request.POST)
         if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/success/')
+        return render(request, self.template_name, {'form': form})
         
