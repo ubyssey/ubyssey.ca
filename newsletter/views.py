a02dfd49cb4bced141b35bb6023bcb82f59d14fa
Keegan Landrigan <keeganland@gmail.com>
2020-07-28 23:25:09 +0000

    correct template name and return render in view

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .forms import SubscriberForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,12 +9,12 @@ class SubscriberFormView(View):
     form_class = SubscriberForm
     
     # Template that will embed the form
-    template_name = 'newsletter_form.html'
+    template_name = 'newsletter/subscribe.html'
 
     #Need to link this to the frontend stuff David made, somehow
     def get(self, request, *args, **kwargs):
         form = self.form_class
-        return render(request, self.template_name)
+        return render(request, self.template_name, {'form': form})
     def post(self, request, *args, **kwards):
         form = self.form_class(request.POST)
         if form.is_valid():
