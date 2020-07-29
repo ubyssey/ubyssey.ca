7e4f1f370f32147cda076c11fb215b223057e0a4
Keegan Landrigan <keeganland@gmail.com>
2020-07-29 18:27:00 +0000

    revise SubscriberForm to BSModalModelForm

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
-from newsletter.models import Subscriber
+#newsletter/forms.py
 
-class SubscriberForm(ModelForm):
+from .models import Subscriber
+from bootstrap_modal_forms.forms import BSModalModelForm
+
+class SubscriberForm(BSModalModelForm):
+    """
+    Allows the creation of Email Subscribe form submittable without navigating to a user
+
+    Depends on django-bootstrap-modal-forms==2.0.0
+    """
     class Meta:
         model = Subscriber
         fields = ['email']
