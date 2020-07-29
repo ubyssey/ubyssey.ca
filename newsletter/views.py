8fadd0b0eacdcc6ed6c6da9450e4f4c360517eca
Keegan Landrigan <keeganland@gmail.com>
2020-07-29 18:59:33 +0000

    BSModalCreateView in newsletter/views.py

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.urls import reverse_lazy
 from .forms import SubscriberForm
+from .models import Subscriber
 from django.http import HttpResponseRedirect
+from bootstrap_modal_forms.generic import BSModalCreateView
 
 # Create your views here.
 
-class SubscriberFormView(View):
+class SubscriberCreateView(BSModalCreateView):
     form_class = SubscriberForm
-    
-    # Template that will embed the form
     template_name = 'newsletter/subscribe.html'
+    success_message = 'Success! Thank you for subscribing!'
+    success_url = reverse_lazy('index')
 
-    #Need to link this to the frontend stuff David made, somehow
-    def get(self, request, *args, **kwargs):
-        form = self.form_class
-        return render(request, self.template_name, {'form': form})
-    def post(self, request, *args, **kwards):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/success/')
-        return render(request, self.template_name, {'form': form})
-        
+# class SubscriberFormView(View):
+#     form_class = SubscriberForm
+    
+#     # Template that will embed the form
+#     template_name = 'newsletter/subscribe.html'
+
+#     def get(self, request, *args, **kwargs):
+#         form = self.form_class
+#         return render(request, self.template_name, {'form': form})
+#     def post(self, request, *args, **kwards):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/success/')
+#         return render(request, self.template_name, {'form': form})
