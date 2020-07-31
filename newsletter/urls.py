c5897470abb301d1e573a36f88e4b110c0c66c99
Keegan Landrigan <keeganland@gmail.com>
2020-07-31 00:45:50 +0000

    Fully functioning subscribe modal

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -5,7 +5,5 @@ from . import views
 
 app_name = 'newsletter'
 urlpatterns = [
-    # path('', views.Index.as_view(), name='index'),
     path('subscribe/', views.SubscriberCreateView.as_view(), name='subscribe'),
-    path('success/', views.SuccessView.as_view(), name='success'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,33 +1,33 @@
-from django.shortcuts import render
-from django.views.generic import View, TemplateView
-from django.urls import reverse_lazy
+import json
 from .forms import SubscriberForm
 from .models import Subscriber
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import View, TemplateView
 from bootstrap_modal_forms.generic import BSModalCreateView
-
-# Create your views here.
 
 class SubscriberCreateView(BSModalCreateView):
     form_class = SubscriberForm
     template_name = 'newsletter/subscribe.html'
     # success_message = 'Success! Thank you for subscribing!'
-    success_url = reverse_lazy('success')
-    
-class SuccessView(TemplateView):
-    template_name = 'newsletter/success.html'
+    # success_url = reverse_lazy('success')
 
-# class SubscriberFormView(View):
-#     form_class = SubscriberForm
-    
-#     # Template that will embed the form
-#     template_name = 'newsletter/subscribe.html'
+    def post(self, request, *args, **kwards):
+        response_data = {} #dict that will hold stats of the submitted subscriber
+        form = self.get_form()
+        if form.is_valid():
+            subscribers_email = request.POST.get('email')        
+            subscriber = Subscriber(email=subscribers_email)
+            subscriber.save()
 
-#     def get(self, request, *args, **kwargs):
-#         form = self.form_class
-#         return render(request, self.template_name, {'form': form})
-#     def post(self, request, *args, **kwards):
-#         form = self.form_class(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect('/success/')
-#         return render(request, self.template_name, {'form': form})
+            response_data['result'] = 'Subscriber added!'
+            response_data['subscriberpk'] = subscriber.pk
+            response_data['email'] = subscriber.email
+            # response_data['created'] = subscriber.created.strftime('%B %d, %Y %I:%M %p') #consider adding 'created' field so we can tell how old a subscriber is
+        else:
+            response_data['result'] = 'form.is_valid() check failed!'
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
