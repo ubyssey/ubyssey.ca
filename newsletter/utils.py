65cb330b5b7a86fb19e0ed156eafa6e4833fa3ec
Keegan Landrigan <keeganland@gmail.com>
2020-08-05 18:52:32 +0000

    init utils.py in newsletter

---
+++ newsletter/utils.py
@@ -0,0 +1,13 @@
+import threading
+import mailchimp
+import mailchimp-marketing
+from django.conf import settings
+
+"""
+For some tutorials on using mailchimp and django together, see:
+https://djangopy.org/package-of-week/how-to-integrate-mailchimp-on-django-to-increase-subscribers/
+
+
+See also:
+https://mailchimp.com/developer/guides/create-your-first-audience
+"""
