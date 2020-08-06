65e40376e35873d54a3f56c9efc25e84eede9ac2
Keegan Landrigan <keeganland@gmail.com>
2020-08-06 22:13:09 +0000

    add temporary webhook endpoint

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'newsletter'
 urlpatterns = [
     path('subscribe/', views.SubscriberCreateView.as_view(), name='subscribe'),
+    path('temporary_webhook_endpoint', views.WebhookResponseHandlerView.as_view(), name='webhook_endpoint')
 ]
