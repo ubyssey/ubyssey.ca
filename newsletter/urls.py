a2a5da3d3f5ded7745bd21d65761a5b531b557ff
Keegan Landrigan <keeganland@gmail.com>
2020-08-13 20:01:11 +0000

    remove deleted view from urls

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -6,6 +6,5 @@ from . import views
 
 app_name = 'newsletter'
 urlpatterns = [
-    path('subscribe/', views.SubscriberCreateView.as_view(), name='subscribe'),
     path(settings.MAILCHIMP_WEBHOOK_ENDPOINT_URL, views.WebhookResponseHandlerView.as_view(), name='webhook')
 ]
