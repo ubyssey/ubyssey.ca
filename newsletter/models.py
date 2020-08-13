785ab2a7a6b7f572cb79413262a0e6b432ad0171
Keegan Landrigan <keeganland@gmail.com>
2020-08-13 20:06:24 +0000

    remove obselete comment

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,19 +1,3 @@
-'''
-Design: interact with this form from ubyssey/js/components/Cookies/Splash.jsx 
-
-<div className='subscribe-form-splash-container'>
-    <form id='subscribe-form-splash'>
-    <input type='text' id='subscriber-email-splash' name='subscriber-email-splash' placeholder='Enter email...'></input>
-    <button type='submit' form='subscribe-form-splash' value='subscribe'>Subscribe</button>
-    </form>
-    <button
-    className='already-sub-button'
-    onClick={() => this.disableSubscribeSplash()}>
-    Already Subscribed?
-    </button>
-</div>
-'''
-
 from django.db import models
 
 # Create your models here.
