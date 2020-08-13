581272775f35b6946922cc309774aba1ec37e7e3
Keegan Landrigan <keeganland@gmail.com>
2020-08-13 21:04:34 +0000

    remove static and templates from newsletter app

--- newsletter/static/newsletter/js/subscribe_modal.js
+++
@@ -1,8 +0,0 @@
-//For django-bootstrap-modal-forms
-$(document).ready(function() {
-
-    $("#subscribe-button").modalForm({
-        //formURL: "{% url 'subscribe' %}"
-        formURL: "/newsletter/subscribe/"
-    });
-});
