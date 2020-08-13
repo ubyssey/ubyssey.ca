5427e0cae78e2d99b9d7c8e5a4f9d7b5d499fc60
Keegan Landrigan <keeganland@gmail.com>
2020-08-13 19:56:48 +0000

    WebhookResponseHandlerView only

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,69 +1,11 @@
 import json
-from .forms import SubscriberForm
 from .models import Subscriber
-from .utils import MailchimpService
-from django.core.exceptions import ValidationError
-from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseServerError
-from django.views.generic import DeleteView, View
-from bootstrap_modal_forms.generic import BSModalCreateView
-
-class SubscriberCreateView(BSModalCreateView):
-    """
-    Class view for creating Subscribers through a form embedded in a bootstrap modal.
-    Bootstrap modal and AJAX are intended to let this view to be used as long as a modal can be triggered, without refreshing or redirecting the page, so a user doesn't lose what they were reading.
-
-    TODO: This should be marked something that needs a refactor or redesign; it may be "overdesigned"
-    What happens is an email first goes to the site's server for validation, then gets posted to Webchimp.
-    Webchimp's webhooks then sync to our db
-    """
-    form_class = SubscriberForm
-    template_name = 'newsletter/subscribe.html'
-
-    def setup(self, request, *args, **kwargs):
-        self.mailchimp_service_object = MailchimpService()
-        return super().setup(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        """
-        POST request is expected to use AJAX and will not redirect user to a different page.
-        It gives a backend integrity check for the data before mailchimp gets its hands on it
-
-        This was based on this blog post (for reference): https://realpython.com/django-and-ajax-form-submissions/
-        """
-        status = 200
-        response_data = {} # Dict that will hold stats of the submitted subscriber, or else let us know something odd happened, like a malformed POST request being made to the URL corresponding to this view
-        form = self.get_form()
-        if form.is_valid():
-            response_data['email'] = request.POST.get('email')
-            subscriber = Subscriber(email=response_data['email'])
-            try:
-                # subscriber.save() #Doesn't need to be saved yet, mailchimp will sync with it later
-                # response_data['created'] = subscriber.created.strftime('%B %d, %Y %I:%M %p') #consider adding 'created' field so we can tell how old a subscriber is
-
-                self.mailchimp_service_object.add_subscriber_to_list(subscriber=subscriber)
-                response_data['result'] = 'Subscriber added!'
-            except IntegrityError:
-                response_data['result'] = 'IntegrityError. Check whether this email is already in the mailing list, or else whether it is properly formed!'
-                status = 400                     
-            except ValidationError:
-                response_data['result'] = "ValidationError"
-                status = 400
-        else:
-            response_data['result'] = 'form.is_valid() check failed!'            
-            status = 400
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json",
-            status=status
-        )
-
-class SubscriberDeleteView(DeleteView):
-    form_class = SubscriberForm
+from django.views.generic import View
 
 class WebhookResponseHandlerView(View):    
     """
-    Responds to webhook requests sent by Mailchimp; keeps their db in sync with ours
+    Responds to webhook requests sent by Mailchimp; keeps our databases in sync
 
     Based on https://mailchimp.com/developer/guides/sync-audience-data-with-webhooks/, adapting Flask to Django
     """
@@ -88,10 +30,15 @@ class WebhookResponseHandlerView(View):
             return HttpResponse(
                 json.dumps(response_data),
                 content_type="application/json"
-            )
-            
+            )            
         except KeyError:
             response_data['result'] = 'Malformed data!'
+            return HttpResponseServerError(
+                json.dumps(response_data),
+                content_type="application/json"
+            )
+        except Exception:
+            response_data['result'] = 'Received well formed data, but something still went wrong!!'
             return HttpResponseServerError(
                 json.dumps(response_data),
                 content_type="application/json"
