7fffd194e50c0bbe53f1dcff0b7fc38f9a1358db
Keegan Landrigan <keeganland@gmail.com>
2020-08-23 18:49:00 +0000

    custom wagtail user logic

--- dispatchwagtail/apps.py
+++ dispatchwagtail/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+import environ
 
 class DispatchwagtailConfig(AppConfig):
     name = 'dispatchwagtail'
+    path = environ.Path(__file__) - 1
--- dispatchwagtail/forms.py
+++ dispatchwagtail/forms.py
@@ -1,9 +1,18 @@
 from django import forms
+from dispatch.models import Person
 
 from wagtail.users.forms import UserEditForm, UserCreationForm
 
 class DispatchUserEditForm(UserEditForm):
-    pass
+    """
+    See: https://docs.wagtail.io/en/v2.10/advanced_topics/customisation/custom_user_models.html
+    """
+    person = forms.ModelChoiceField(queryset=Person.objects, required=True, label="Who are you?")
+
 
 class DispatchUserCreationForm(UserCreationForm):
-    pass
+    """
+    See: https://docs.wagtail.io/en/v2.10/advanced_topics/customisation/custom_user_models.html
+    """
+    person = forms.ModelChoiceField(queryset=Person.objects, required=True, label="Who are you?")
+    is_active = forms.BooleanField(required=False)
