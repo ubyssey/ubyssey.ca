6c05dde20aff5cd8d02a3721a6514e4513d2225f
Keegan Landrigan <keeganland@gmail.com>
2020-08-23 18:50:41 +0000

    is_active on both forms

--- dispatchwagtail/forms.py
+++ dispatchwagtail/forms.py
@@ -8,7 +8,7 @@ class DispatchUserEditForm(UserEditForm):
     See: https://docs.wagtail.io/en/v2.10/advanced_topics/customisation/custom_user_models.html
     """
     person = forms.ModelChoiceField(queryset=Person.objects, required=True, label="Who are you?")
-
+    is_active = forms.BooleanField(required=False)
 
 class DispatchUserCreationForm(UserCreationForm):
     """
