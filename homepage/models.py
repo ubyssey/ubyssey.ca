ba783e6785c3674d8b47f1f58d66f5d9cd45b83f
Keegan Landrigan <keeganland@gmail.com>
2020-09-02 10:42:43 +0000

    homepage Page basics

--- homepage/models.py
+++ homepage/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
     template = 'homepage/base.html'
+    max_count = 1
+    banner_title = models.CharField(max_length=100, blank=False, null=False)
+
+    content_panels = Page.content_panels + [
+        #This is a very standard necessary in all Page objects in Wagtail
+        FieldPanel('banner_title'),
+    ]
