c4f80ff0ae874a955b075e579d068885fb99d5dc
Keegan Landrigan <keeganland@gmail.com>
2020-09-02 12:32:01 +0000

    init various block types

--- streams/blocks.py
+++ streams/blocks.py
@@ -2,3 +2,9 @@ from wagtail.core import blocks
 
 class FrontpageBlock(blocks.StructBlock):
     pass
+
+class ArticlesListBlock(blocks.ListBlock):
+    pass
+
+class SidebarBlock(blocks.StreamBlock):
+    pass
