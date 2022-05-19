3650d7a555a07f8f8be84c577b0e3779acd0416a
Mei Chi <meichi.chin98@gmail.com>
2022-05-19 02:09:19 +0000

    main.js docu work in progress

--- ubyssey/static_src/src/js/main.js
+++ ubyssey/static_src/src/js/main.js
@@ -7,8 +7,8 @@ import upcomingEvents from './widgets/upcoming-events';
   initializeSocialMediaActions();
 
   ubysseyHeaderMobilePopUp();
-  ubysseyHeaderMagazineDropDown();
-  ubysseyHeaderCultureDropDown();
+  ubysseyHeaderMagazineDropDown(); // appears to be custom js for Magazine menu dropdown Keegan has talked about
+  ubysseyHeaderCultureDropDown();  // appears to be custom js for Culture menu dropdown
   archiveMobileDropDown();
 
   if ($('.js-article').length) {
@@ -63,13 +63,20 @@ import upcomingEvents from './widgets/upcoming-events';
 
 })();
 
+
 function disableScroll() {
+  // Triggered once for each movement
+  // Continue to be triggered until finger is released
+  // Default touchmove behaviour prevented. Only for touchscreen.
   $(document).on('touchmove', function (e) {
     e.preventDefault();
   });
   $('body').addClass('u-no-scroll');
 }
 
+// jQuery
+// Remove event handler for touchmove, default touchmove behaviour enabled
+// Remove class u-no-scroll
 function enableScroll() {
   $(document).off('touchmove');
   $('body').removeClass('u-no-scroll');
@@ -116,19 +123,27 @@ function issueParser() {
   req.send();
 }
 
+// If hovering over sections-more-dropdown element,
+// element slides up or down.
+// When no longer hovering, hides element sections-more-dropdown
 function ubysseyHeaderMagazineDropDown() {
   $('#sections-more-dropdown').hover(function (e) {
+    // inFunction (triggered when mouse enters)
     e.stopPropagation();
-    $('.sections-more').finish();
-    $('.sections-more').slideToggle(300);
+    $('.sections-more').finish();  // finishes/stops/removes all current animation
+    $('.sections-more').slideToggle(300); // through slideUp() and slideDown() functions, if in up position then slides down and vice versa
   }, (function (e) {
+    // outFunction (triggered when mouse leaves)
     e.stopPropagation();
-    $('.sections-more').finish();
-    $('.sections-more').fadeOut(300);
+    $('.sections-more').finish(); // finishes/stops/removes all current animation
+    $('.sections-more').fadeOut(300); // gradually changes the opacity from visible to hidden (fading effect)
   })
   );
 }
 
+// If hovering over culture-sections-more-dropdown element,
+// element slides up or down.
+// When no longer hovering, hides element culture-sections-more-dropdown
 function ubysseyHeaderCultureDropDown() {
   $('#culture-sections-more-dropdown').hover(function (e) {
     e.stopPropagation();
@@ -145,6 +160,7 @@ function ubysseyHeaderCultureDropDown() {
 function archiveMobileDropDown() {
   let DROPDOWN_FADE_TIME = 100;
 
+  // on click, element parent fades (visible to hidden)
   $('.js-dropdown-container').click(function (e) {
     e.preventDefault();
     var dropdown = $(this).parent();
@@ -174,12 +190,16 @@ function archiveMobileDropDown() {
 }
 
 function ubysseyHeaderMobilePopUp() {
+  // on clicking on the a element w/ class=='menu'
   $('a.menu').click(function (e) {
     e.preventDefault();
+    // if navigation bar is visible, hide it
     if ($('nav.mobile').is(':visible')) {
       $('nav.mobile').hide();
       $(this).removeClass('active');
     } else {
+      // if search-form is visible, hide it
+      // and show navigation bar instead
       if ($('#search-form').is(':visible')) {
         $('#search-form').hide();
         $('a.search').removeClass('active');
@@ -208,12 +228,18 @@ function initializeSearch() {
 }
 
 function initializeSearchFormActions() {
+
+  // on clicking the a element w/ class=='search'
+  // if the search form is already visible, hide search form
+  // if on mobile, hide navigation bar and show search form
   $(document).on('click', 'a.search', function (e) {
     e.preventDefault();
     if ($('#search-form').is(':visible')) {
       $('#search-form').hide();
       $(this).removeClass('active');
     } else {
+      // if searching on mobile
+      // hide search me
       if ($('nav.mobile').is(':visible')) {
         $('nav.mobile').hide();
         $('a.menu').removeClass('active');
@@ -222,10 +248,12 @@ function initializeSearchFormActions() {
       $('#search-bar').focus();
       $(this).addClass('active');
     }
-    e.stopPropagation();
+    e.stopPropagation(); // prevents propagation (i.e. to parent/child elements) of the same event from being called
   });
 
   //listeners for hiding search form
+
+  // (1) Hitting the ESCAPE button would hide the search form
   $(document).on('keyup', function (e) {
     var ESCAPE = 27;
     if (e.keyCode == ESCAPE) {
@@ -233,16 +261,22 @@ function initializeSearchFormActions() {
     }
   });
 
+  // (2) Clicking on the document would hide the search form
+  // possible error? should be clicking on document not incl. the search form itself?
   $(document).click(function () {
     $('#search-form').hide();
   });
 
+  // Propagation of clicking on all .u-container elements within search-form are stopped
   $(document).on('click', '#search-form > .u-container', function (e) {
     e.stopPropagation();
   });
 }
 
 function initializeSocialMediaActions() {
+  // on clicking on the a element w/ class=='facebook'
+  // likes the page
+  // source: https://washamdev.com/facebook-sdk-javascript-tutorial-graph-api-facebook-login/
   $(document).on('click', 'a.facebook', function (e) {
     e.preventDefault();
     FB.ui({
@@ -254,12 +288,14 @@ function initializeSocialMediaActions() {
     }, function (response) { });
   });
 
+  // on clicking on the a element w/ class=='twitter'
   $(document).on('click', 'a.twitter', function (e) {
     e.preventDefault();
     window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow',
       'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
   });
 
+  // on clicking on the a element w/ class=='reddit'
   $(document).on('click', 'a.reddit', function (e) {
     e.preventDefault();
     window.open('http://www.reddit.com/submit?url=' + $(this).data('url') + '&title=' + $(this).data('title') + '&', 'redditwindow',
