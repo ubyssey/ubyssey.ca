88eda8c06f34544b6939635c8a8513e4a1e01a64
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2022-11-26 22:09:02 -0800

    Adding the strikethrough feature
    I am adding a wagtail_hooks.py that has a function that will add a strikethrough HTML element and rich-text element online.
    
    Th current issue is that I am unable to move the strikethrough button to the right of the page.

---
+++ article/wagtail_hooks.py
@@ -0,0 +1,42 @@
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+from wagtail.core import hooks
+
+
+# 1. Use the register_rich_text_features hook.
+@hooks.register('register_rich_text_features')
+def register_Strikethrough_feature(features):
+    """
+    Registering the `mark` feature, which uses the `MARK` Draft.js inline style type,
+    and is stored as HTML with a `<mark>` tag.
+    """
+    feature_name = 'STRIKETHROUGH'
+    type_ = 'STRIKETHROUGH'
+    tag = 'STRIKETHROUGH'
+
+    # 2. Configure how Draftail handles the feature in its toolbar.
+    control = {
+        'type': type_,
+        'label': '-!!!',
+        'description': 'STRIKETHROUGH',
+        # This isn’t even required – Draftail has predefined styles for MARK.
+        # 'style': {'textDecoration': 'line-through'},
+    }
+
+    # 3. Call register_editor_plugin to register the configuration for Draftail.
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    # 4.configure the content transform from the DB to the editor and back.
+    db_conversion = {
+        'from_database_format': {tag: InlineStyleElementHandler(type_)},
+        'to_database_format': {'style_map': {type_: tag}},
+    }
+
+    # 5. Call register_converter_rule to register the content transformation conversion.
+    features.register_converter_rule('contentstate', feature_name, db_conversion)
+
+    # 6. (optional) Add the feature to the default features list to make it available
+    # on rich text fields that do not specify an explicit 'features' list
+    features.default_features.append('strikethrough')
