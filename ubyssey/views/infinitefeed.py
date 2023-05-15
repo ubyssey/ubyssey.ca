97a9e30a5baedc1afd92aed3d7a69e5f64f5f6c3
tubbdoose <samuel.m.d.low@gmail.com>
2023-05-15 20:32:09 +0000

    Created crude infinite scroll feature

--- ubyssey/views/infinitefeed.py
+++ ubyssey/views/infinitefeed.py
@@ -2,20 +2,19 @@ from article.models import ArticlePage
 from django.http import HttpResponse
 from django.core import serializers
 import json
+from django.template import loader
 def infinitefeed(request):
     if request.method == 'GET':
         section = request.GET['section']
         start = request.GET['start']
         number = request.GET['number']
         articles = ArticlePage.objects.live().public().filter(current_section=section).order_by('-explicit_published_at')[int(start):int(start)+int(number)]
+        
         if len(articles) == 0:
-             return HttpResponse("End of feed")
+            return HttpResponse("End of feed")
         else:
-            data = []
-            for article in articles:
-                data.append({"name": article.title, "author": article.authors_string})
-            response = {}
-            response['result'] = data
-            return HttpResponse(json.dumps(response), content_type="application/json")
+            template = loader.select_template(["article/infinitefeed.html"])
+            data = {'articles': articles}
+            return HttpResponse(loader.render_to_string("article/infinitefeed.html", data))
     else:
-           return HttpResponse("Request method is not a GET")
+            return HttpResponse("Request method is not a GET")
