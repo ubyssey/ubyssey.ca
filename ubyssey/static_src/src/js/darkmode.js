dcf0cd275cca3858e0de2441d0f7588478024e88
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-05-30 12:43:03 +0000

    Stable Version (except for the ubyssey logo): Storage of the dark mode on the user's laptop is working

--- ubyssey/static_src/src/js/darkmode.js
+++ ubyssey/static_src/src/js/darkmode.js
@@ -6,16 +6,17 @@
   })();
 
 function darkState() {
-    $(document).on('onload', function (e) {
+    window.onload = (event) => {
         storedMode = localStorage.getItem("darkMode");
-    
+        
         if (storedMode == null) {
             localStorage.setItem("darkMode", getDarkMode());
     
         } else {
             document.querySelector('meta[name="color-scheme"]').setAttribute("content", storedMode);
+            setDarkMode(storedMode);
         }
-    });
+      };
   
 }
   
@@ -25,6 +26,29 @@ function getDarkMode() {
     return document.querySelector('meta[name="color-scheme"]').content;
 }
   
+
+function setDarkMode(mode) {
+    if (mode == "light") {
+        // nav bar
+        var r = document.querySelector(':root');
+        r.style.setProperty('--background', 'rgba(255, 255, 255, 0.99)');
+        r.style.setProperty('--nav-mobile-background-barely-transparent', 'rgba(249, 249, 249, 0.99)');
+        r.style.setProperty('--nav-h3-color', '#5D5D5D');
+        r.style.setProperty('--nav-span-color', '#33331E');
+        r.style.setProperty('--nav-headline-h1', '#373737');
+        r.style.setProperty('--three-bar-logo-color', '#303030');
+    } else if (mode == "dark") {
+        // nav bar
+        var r = document.querySelector(':root');
+        r.style.setProperty('--background', 'rgba(0, 0, 0, 0.99)');
+        r.style.setProperty('--nav-mobile-background-barely-transparent', 'rgba(1, 1, 1, 0.99)');
+        r.style.setProperty('--nav-h3-color', '#5D5D5D');
+        r.style.setProperty('--nav-span-color', '#FFFFF');
+        r.style.setProperty('--nav-headline-h1', '#5D5D5D');
+        r.style.setProperty('--three-bar-logo-color', '#EFEFEF');
+    }
+}
+
 function DarkModeToggle() {
 
     $(document).on('click', '.dark-mode-switcher', function (e) {
@@ -33,25 +57,18 @@ function DarkModeToggle() {
         if (mode == "dark") {
             document.querySelector('meta[name="color-scheme"]').setAttribute("content", "light");
             mode = "light"
-            // nav bar
-            var r = document.querySelector(':root');
-            r.style.setProperty('--background', 'rgba(255, 255, 255, 0.99)');
-            r.style.setProperty('--nav-mobile-background-barely-transparent', 'rgba(249, 249, 249, 0.99)');
-            r.style.setProperty('--nav-h3-color', '#5D5D5D');
-            r.style.setProperty('--nav-span-color', '#33331E');
-            r.style.setProperty('--nav-headline-h1', '#373737');
-            r.style.setProperty('--three-bar-logo-color', '#303030');
+            
+            localStorage.setItem("darkMode", mode);
+            
+            setDarkMode(mode);
+            
         } else if (mode == "light") {
             document.querySelector('meta[name="color-scheme"]').setAttribute("content", "dark");
             mode = "dark"
-            // nav bar
-            var r = document.querySelector(':root');
-            r.style.setProperty('--background', 'rgba(0, 0, 0, 0.99)');
-            r.style.setProperty('--nav-mobile-background-barely-transparent', 'rgba(1, 1, 1, 0.99)');
-            r.style.setProperty('--nav-h3-color', '#5D5D5D');
-            r.style.setProperty('--nav-span-color', '#FFFFF');
-            r.style.setProperty('--nav-headline-h1', '#5D5D5D');
-            r.style.setProperty('--three-bar-logo-color', '#EFEFEF');
+            
+            localStorage.setItem("darkMode", mode);
+            
+            setDarkMode(mode);
         }
     });
 }
