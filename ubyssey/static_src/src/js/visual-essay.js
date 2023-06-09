cd7f720c0028ca66ac4d97142fbb22f8e8c6cc50
Samuel <samuel.m.d.low@gmail.com>
2023-06-09 08:58:12 +0000

    Fixed visual essay styling

--- ubyssey/static_src/src/js/visual-essay.js
+++ ubyssey/static_src/src/js/visual-essay.js
@@ -1,28 +1,31 @@
 document.body.onscroll = function () {
-    var text = document.getElementById("o-visual-essay__text-container");
-    var imagescontainer = document.getElementById("o-visual-essay__images-container");
     var count = 0;
-    for(let i=0; i<text.children.length; i++) {
-        //console.log("marks:" + text.children[i].offsetTop);
-        //console.log("scroll:" + document.documentElement.scrollTop);
-        if(text.children[i].classList.contains("mark")){
-            if(text.children[i].offsetTop < document.documentElement.scrollTop + document.body.offsetHeight/2) {
-            count = count + 1;
+
+    var containers = document.getElementsByClassName("o-visual-essay__content-container");
+    for(let i=0; i < containers.length; i++) {
+
+        var left = containers[i].getElementsByClassName("o-visual-essay__left-container")[0];
+        var right = containers[i].getElementsByClassName("o-visual-essay__right-container")[0];
+
+        for(let i=0; i<right.children.length; i++) {
+            if(right.children[i].classList.contains("mark")){
+                if(right.children[i].offsetTop < document.documentElement.scrollTop + document.body.offsetHeight/2) {
+                count = count + 1;
+                }
             }
         }
-    }
-    
-    if(count == 0) {
-        count = 1;
-    }
+        
+        if(count == 0) {
+            count = 1;
+        }
 
-    for(let a=0; a<imagescontainer.children.length; a++) {
-        //console.log("count: " + count);
-        if(a==count-1){
-            console.log("a:" + a)
-            imagescontainer.children[a].style.opacity = "1";
-        } else {
-            imagescontainer.children[a].style.opacity = "0";
+        for(let a=0; a<left.children.length; a++) {
+            //console.log("count: " + count);
+            if(a==count-1){
+                left.children[a].style.opacity = "1";
+            } else {
+                left.children[a].style.opacity = "0";
+            }
         }
     }
 };
