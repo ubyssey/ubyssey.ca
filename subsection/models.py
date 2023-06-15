ce37a016904aafbafa7b414e66aab67b24502052
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-06-15 10:05:09 +0000

    Removed  Unneccesary Functions

--- subsection/models.py
+++ subsection/models.py
@@ -25,8 +25,6 @@ from wagtail.snippets.models import register_snippet
 
 class SubSectionPage(RoutablePageMixin, SectionablePage):
 
-
-
     parent_page_types = [
         'home.HomePage',
         'section.SectionPage',
@@ -38,66 +36,11 @@ class SubSectionPage(RoutablePageMixin, SectionablePage):
 
     show_in_menus_default = True
 
-
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        search_query = request.GET.get("q")
-        page = request.GET.get("page")
-        order = request.GET.get("order")
 
-        if order == 'oldest':
-            article_order = "explicit_published_at"
-        else:            
-            article_order = "-explicit_published_at"
-        context["order"] = order
-
-        all_articles = self.get_section_articles(order=article_order)
-        if 'category_slug' in kwargs:            
-            all_articles = all_articles.filter(category__slug=kwargs['category_slug'])
-
-        context["featured_articles"] = self.get_featured_articles()
-
-        if search_query:
-            context["search_query"] = search_query
-            all_articles = all_articles.search(search_query)
-
-        # Paginate all posts by 15 per page
-        paginator = Paginator(all_articles, per_page=15)       
-        try:
-            # If the page exists and the ?page=x is an int
-            paginated_articles = paginator.page(page)
-            context["current_page"] = page
-
-        except PageNotAnInteger:
-            # If the ?page=x is not an int; show the first page
-            paginated_articles = paginator.page(1)
-           
-        except EmptyPage:
-            # If the ?page=x is out of range (too high most likely)
-            # Then return the last page
-            paginated_articles = paginator.page(paginator.num_pages)
-
-        context["paginated_articles"] = paginated_articles #this object is often called page_obj in Django docs, but Page means something else in Wagtail
-    
         return context
     
-    def get_section_articles(self, order='-explicit_published_at') -> QuerySet:
-        # return ArticlePage.objects.from_section(section_root=self)
-        section_articles = ArticlePage.objects.live().public().filter(current_section=self.slug).order_by(order)
-        return section_articles
-
-    def get_featured_articles(self, queryset=None, number_featured=4) -> QuerySet:
-        """
-        Returns a truncated queryset of articles
-            queryset: if not included, will default to all live, public, ArticlePage descendents of this SectionPage
-            number_featured: defaults to 4 as brute fact about our template's design
-        """
-        if queryset == None:
-            # queryset = ArticlePage.objects.from_section(section_root=self)
-            queryset = self.get_section_articles()
-        return queryset[:number_featured]    
-    featured_articles = property(fget=get_featured_articles)
-
     def save(self, *args, **kwargs):
         self.current_section = self.slug
         return Page.save(self,*args, **kwargs)
