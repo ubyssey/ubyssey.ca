38c8970332cd990dbf0f87a0deaf940ca7d8cf16
tubbdoose <samuel.m.d.low@gmail.com>
2023-06-22 18:28:41 +0000

    Moved sidebar js to infinitefeed js

--- ubyssey/static_src/src/js/infinitefeed.js
+++ ubyssey/static_src/src/js/infinitefeed.js
@@ -16,7 +16,7 @@ getData("search_query");
 getData("label");
 
 function getArticles() {
-  loader.setAttribute("inactive", "True");
+  loader.classList.remove("hide");
   
   data["start"] = loader.getAttribute("start");
   data["number"] = loader.getAttribute("number");
@@ -37,10 +37,14 @@ function recievedata(data) {
   var feed = document.getElementById("feed");
   if(data == "End of feed") {
     console.log("end of feed");
-    loader.remove();
-    clearInterval(loadFeed);
+    loader.setAttribute("inactive", "True");
+    var congratz = document.createElement("p");
+    congratz.innerHTML = "You reached the end! 🥳";
+    loader.replaceChildren(congratz);
   } else {
+    loader.setAttribute("inactive", "True");
     feed.insertAdjacentHTML("beforeend", data);
+    loader.classList.add("hide");
     loader.removeAttribute("inactive");
   }  
 }
@@ -49,12 +53,44 @@ loader.setAttribute("start", "15");
 loader.setAttribute("number", "15");
 
 
-window.onscroll = function () 
-{
+function loadArticles() 
+{ 
   if(loader.hasAttribute("inactive") == false) {
     if(document.documentElement.scrollTop > loader.offsetTop - document.body.offsetHeight){
       getArticles();
       loader.setAttribute("start", String(parseInt(loader.getAttribute("start"))+ parseInt(loader.getAttribute("number"))));
     }
   }
-};
+}
+
+var sidebar = document.getElementsByClassName("c-sidebar")[0];
+
+function switchSidebar() 
+{
+  var x = Math.floor((document.documentElement.scrollTop- feed.offsetTop) / (1.5*document.body.offsetHeight))% sidebar.children.length;
+  if (x < 0) {
+    x = 0;
+  }
+  if(x != Array.prototype.indexOf.call(sidebar.children,document.getElementsByClassName("c-sidebar_active")[0])){
+    if (document.getElementsByClassName("c-sidebar_active").length > 0) {
+      document.getElementsByClassName("c-sidebar_active")[0].classList.add("c-sidebar_inactive");
+      document.getElementsByClassName("c-sidebar_active")[0].classList.remove("c-sidebar_active");
+
+    }
+    sidebar.children[x].classList.remove("c-sidebar_inactive");
+    sidebar.children[x].classList.add("c-sidebar_active");
+  }
+}
+
+if (document.getElementsByClassName("c-sidebar").length > 0) {
+  console.log("gamer");
+  window.onscroll = function() {
+    loadArticles();
+    switchSidebar();
+  }
+} else {
+  console.log("not gamer");
+  window.onscroll = function() {
+    loadArticles();
+  }
+}
