64d558a0845abd356dd1bb4e6dcacdb187ae63fd
Samuel <samuel.m.d.low@gmail.com>
2023-06-22 11:28:07 +0000

    Formatted code

--- ubyssey/views/feed.py
+++ ubyssey/views/feed.py
@@ -13,17 +13,24 @@ class RssFeedWithImage(feedgenerator.Rss201rev2Feed):
         handler.addQuickElement("title", self.feed['title'])
         handler.addQuickElement("link", self.feed['link'])
         handler.addQuickElement("description", self.feed['description'])
+
         if self.feed['feed_url'] is not None:
             handler.addQuickElement("atom:link", None, {"rel": "self", "href": self.feed['feed_url']})
+
         if self.feed['language'] is not None:
             handler.addQuickElement("language", self.feed['language'])
+
         for cat in self.feed['categories']:
             handler.addQuickElement("category", cat)
+
         if self.feed['feed_copyright'] is not None:
             handler.addQuickElement("copyright", self.feed['feed_copyright'])
+
         handler.addQuickElement("lastBuildDate", feedgenerator.rfc2822_date(self.latest_post_date()))
+
         if self.feed['ttl'] is not None:
             handler.addQuickElement("ttl", self.feed['ttl'])
+            
         if self.feed['image_url'] is not None:
             handler.startElement('image',{})
             handler.addQuickElement("url", self.feed['image_url'])
