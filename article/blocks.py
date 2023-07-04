af0c63fe163288ac84a317b3d8808efac25cf174
Samuel <samuel.m.d.low@gmail.com>
2023-07-04 04:03:00 +0000

    Added script block

--- article/blocks.py
+++ article/blocks.py
@@ -13,7 +13,22 @@ class PullQuoteBlock(blocks.StructBlock):
 
 class ChooseSideBlock(blocks.ChoiceBlock):
     choices=[('left', 'Left'),('right', 'Right'),]
-    default="right"
+    required=True
+
+class VEScriptBlock(blocks.StructBlock):
+    script =  blocks.RawHTMLBlock(
+        label = "Raw HTML Block",
+        help_text = "Only Use single quotes. WARNING: DO NOT use this unless you really know what you're doing!"
+    )
+    reverse_script =  blocks.RawHTMLBlock(
+        label = "Raw HTML Block",
+        help_text = "Only Use single quotes. WARNING: DO NOT use this unless you really know what you're doing!"
+    )
+    height = blocks.IntegerBlock(required=True, default=0, min_value=0, max_value=100)
+
+    class Meta:
+        template = 'article/stream_blocks/ve_script_block.html',
+        icon = "cogs"
 
 class VisualEssayBlock(blocks.StructBlock):
     content = blocks.StreamBlock([
@@ -23,13 +38,13 @@ class VisualEssayBlock(blocks.StructBlock):
                     label="Rich Text Block",
                     help_text = "Write your article contents here. See documentation: https://docs.wagtail.io/en/latest/editor_manual/new_pages/creating_body_content.html#rich-text-fields"
                 )),
-                ('side',ChooseSideBlock()),
+                ('side',ChooseSideBlock(default=("right", "Right"))),
             ], icon = "doc-full"
         )),
         ('image', blocks.StructBlock(
             [
                 ('block', image_blocks.ImageBlock()),
-                ('side',ChooseSideBlock()),
+                ('side',ChooseSideBlock(default=("left", "Left"))),
             ], icon = "image"
         )),
         ('video', blocks.StructBlock(
@@ -38,7 +53,7 @@ class VisualEssayBlock(blocks.StructBlock):
                     label = "Credited/Captioned One-Off Video",
                     help_text = "Use this to credit or caption videos that will only be associated with this current article, rather than entered into our video library. You can also embed videos in a Rich Text Block."
                 )),
-                ('side',ChooseSideBlock()),
+                ('side',ChooseSideBlock(default=("left", "Left"))),
             ], icon = "media"
         )),
         ('raw_html', blocks.StructBlock(
@@ -47,15 +62,16 @@ class VisualEssayBlock(blocks.StructBlock):
                     label = "Raw HTML Block",
                     help_text = "WARNING: DO NOT use this unless you really know what you're doing!"
                 )),
-                ('side',ChooseSideBlock()),
+                ('side',ChooseSideBlock(default=("left", "Left"))),
             ], icon = "code"
         )),
         ('quote', blocks.StructBlock(
             [
                 ('block', PullQuoteBlock()),
-                ('side',ChooseSideBlock()),
+                ('side',ChooseSideBlock(default=("left", "Left"))),
             ], icon = "openquote"
         )),
+        ('script_block', VEScriptBlock()),
     ])
 
     class Meta:
--- ubyssey/static_src/src/js/visual-essay.js
+++ ubyssey/static_src/src/js/visual-essay.js
@@ -4,8 +4,8 @@ document.body.onscroll = function () {
     var containers = document.getElementsByClassName("o-visual-essay__content-container");
 
     for(let i=0; i < containers.length; i++) {
-        // containers[i].offsetTop > document.documentElement.scrollTop + document.body.offsetHeight/2 || 
-        if (containers[i].offsetTop + containers[i].offsetHeight < document.documentElement.scrollTop + document.body.offsetHeight/2) {
+        var scrolled = document.documentElement.scrollTop + document.body.offsetHeight/2;
+        if (containers[i].offsetTop + containers[i].offsetHeight < scrolled) {
             containers[i].classList.remove("o-visual-essay__content-container-show");
             containers[i].classList.add("o-visual-essay__content-container-hide");
             var left = containers[i].getElementsByClassName("o-visual-essay__left-container")[0];
@@ -23,9 +23,24 @@ document.body.onscroll = function () {
             var right = containers[i].getElementsByClassName("o-visual-essay__right-container")[0];
     
             for(let i=0; i<right.children.length; i++) {
-                if(right.children[i].classList.contains("mark")){
-                    if(right.children[i].offsetTop < document.documentElement.scrollTop + document.body.offsetHeight/2) {
-                    count = count + 1;
+                var block = right.children[i];
+                if(block.classList.contains("script_block")){
+                    if (block.getAttribute("activated") == "false") {
+                        if(block.offsetTop < scrolled) {
+                            eval(block.getAttribute("script"));
+                            block.setAttribute("activated", "true");
+                        }
+                    } else {
+                        if(block.offsetTop > scrolled) {
+                            eval(block.getAttribute("reversescript"));
+                            block.setAttribute("activated", "false");
+                        }
+                    }
+                }
+
+                if(block.classList.contains("mark")){
+                    if(block.offsetTop <scrolled) {
+                        count = count + 1;
                     }
                 }
             }
@@ -36,18 +51,27 @@ document.body.onscroll = function () {
     
             for(let a=0; a<left.children.length; a++) {
                 //console.log("count: " + count);
-                if(a==count-1){
+
+                if (count == 0 && a==0) {
+                    left.children[a].classList.add("o-visual-essay__left-first");
+                    left.children[a].classList.remove("o-visual-essay__left-show");
+                    left.children[a].classList.remove("o-visual-essay__left-next");
+                    left.children[a].classList.remove("o-visual-essay__left-prev");
+                } else if(a==count-1){
                     left.children[a].classList.add("o-visual-essay__left-show");
                     left.children[a].classList.remove("o-visual-essay__left-next");
                     left.children[a].classList.remove("o-visual-essay__left-prev");
+                    left.children[a].classList.remove("o-visual-essay__left-first");
                 } else if (a < count-1) {
                     left.children[a].classList.add("o-visual-essay__left-prev");
                     left.children[a].classList.remove("o-visual-essay__left-show");
                     left.children[a].classList.remove("o-visual-essay__left-next");
+                    left.children[a].classList.remove("o-visual-essay__left-first");
                 } else {
                     left.children[a].classList.add("o-visual-essay__left-next");
                     left.children[a].classList.remove("o-visual-essay__left-show");
                     left.children[a].classList.remove("o-visual-essay__left-prev");
+                    left.children[a].classList.remove("o-visual-essay__left-first");
                 }
             }
         }
