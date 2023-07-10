0d0c935302239db9b4fe4da160126b34f4323c2c
Peter Siemens <peterjsiemens@gmail.com>
2023-07-09 23:17:31 -0700

    Replace Dispatch user model with custom user model

---
+++ users/managers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, is_superuser=False):
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_superuser=is_superuser)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        return self.create_user(email, password, is_superuser=True)
