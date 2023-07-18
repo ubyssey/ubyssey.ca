adc5dcc414324c15cdd5c4a9793cfdb21cc943fa
Peter Siemens <peterjsiemens@gmail.com>
2023-07-18 11:34:38 -0700

    Split up initial User model migration to make dispatch_user rename easier
    Steps to migrate Dispatch users to the new custom User model:
    
    1. Manually add users.0001_initial_1 to the "django_migrations" table to prevent this error:
      "django.db.migrations.exceptions.InconsistentMigrationHistory: Migration authtoken.0001_initial is applied before its dependency users.0001_initial_1"
    
    2. Rename "dispatch_user" table to "users_user"
    
    3. Run "python manage.py migrate", which should apply the "users.0002_initial_2" migration

--- users/migrations/0001_initial_1.py
+++ users/migrations/0001_initial_1.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             name='User',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID')),
-                ('email', models.EmailField(max_length=255, unique=True)),
+                ('email', models.CharField(max_length=255, unique=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('is_active', models.BooleanField(default=True)),
