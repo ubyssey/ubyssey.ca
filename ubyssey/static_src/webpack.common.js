40e4b12b9f010f2829ba6f9d5090586b2990a7f5
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-07-25 12:05:40 +0000

    Removed adblocker and cleaned the code

--- ubyssey/static_src/webpack.common.js
+++ ubyssey/static_src/webpack.common.js
@@ -21,7 +21,6 @@ module.exports = {
     'soccer_nationals': './src/js/soccer_nationals.js',
     'timeline': './src/js/timeline.js',
     'episode': './src/js/episode.js',
-    blockadblock: './src/js/blockadblock.js',
     darkmode: './src/js/darkmode.js',
   },
   output: {
