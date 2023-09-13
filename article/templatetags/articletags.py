d21ea1720d4b275bcd23fe9fffa3883804c1b4b6
tubbdoose <samuel.m.d.low@gmail.com>
2023-09-13 07:23:42 +0000

    Added better display of publish date

--- article/templatetags/articletags.py
+++ article/templatetags/articletags.py
@@ -2,6 +2,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.template.loader import render_to_string
 from section.models import SectionPage
+import datetime
 
 register = template.Library()
 
@@ -30,3 +31,37 @@ def get_section_link(value):
 @register.filter(name='get_section_title')
 def get_section_title(value):
     return value.get_parent().title
+
+@register.filter(name='display_pubdate')
+def display_pubdate(value):
+
+    pubdate = value
+    today = datetime.datetime.now().replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-7)))
+    delta = today - pubdate
+
+    if delta.total_seconds() > datetime.timedelta(days=365).total_seconds():
+        return pubdate.strftime("%b. xx%d %y").replace("xx0","")
+    elif delta.total_seconds() > datetime.timedelta(days=1).total_seconds():
+        return pubdate.strftime("%b. xx%d").replace("xx0","")
+    elif delta.total_seconds() > datetime.timedelta(hours=5).total_seconds():
+        if today.date() == pubdate.date():
+            return "Today at " + pubdate.strftime("xx%I:%M %p").replace("xx0","")
+        return "Yesterday at " + pubdate.strftime("xx%I:%M %p").replace("xx0","")
+
+    elif delta.total_seconds() > datetime.timedelta(hours=1).total_seconds():
+        hours = round(delta.total_seconds()/3600)
+        if hours == 1:
+            return "1 hour ago"
+        else:
+            return str(hours) + " hours ago"
+    elif delta.total_seconds() > datetime.timedelta(minutes=1).total_seconds():
+        minutes = round(delta.total_seconds()/60)
+        if minutes == 1:
+            return "1 minute ago"
+        else:
+            return str(minutes) + " minutes ago"
+    
+    seconds = round(delta.total_seconds())
+    if seconds == 1:
+        return "1 second ago"
+    return str(seconds) + " seconds ago"
