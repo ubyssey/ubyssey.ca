ada62554cf4066b2afe4a4493b4533395901841e
tubbdoose <samuel.m.d.low@gmail.com>
2023-09-13 18:42:17 +0000

    Changed to full month spelling

--- article/templatetags/articletags.py
+++ article/templatetags/articletags.py
@@ -41,9 +41,9 @@ def display_pubdate(value):
     delta = today - pubdate
 
     if delta.total_seconds() > datetime.timedelta(days=365).total_seconds():
-        return pubdate.strftime("%b. xx%d %y").replace("xx0","").replace("xx","")
+        return pubdate.strftime("%B xx%d %y").replace("xx0","").replace("xx","")
     elif delta.total_seconds() > datetime.timedelta(days=1).total_seconds():
-        return pubdate.strftime("%b. xx%d").replace("xx0","").replace("xx","")
+        return pubdate.strftime("%B xx%d").replace("xx0","").replace("xx","")
     elif delta.total_seconds() > datetime.timedelta(hours=5).total_seconds():
         if today.date() == pubdate.date():
             return "Today at " + pubdate.strftime("xx%I:%M %p").replace("xx0","").replace("xx","")
