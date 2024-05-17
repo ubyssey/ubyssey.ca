a66d915f282053903abf965dfe5e3d58bd42115a
TubbDoose <samuel.m.d.low@gmail.com>
2024-05-17 00:51:16 +0000

    Add required field to user for django admin, add some important models to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HomePage, TopArticlesOrderable
 
 # Register your models here.
+
+admin.site.register(HomePage)
+admin.site.register(TopArticlesOrderable)
--- section/admin.py
+++ section/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SectionPage
 
 # Register your models here.
+admin.site.register(SectionPage)
