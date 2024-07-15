4c6711510054580fe9874b6f3c9865295fdd912f
Nishim12 <nishimsinghi@gmail.com>
2024-07-15 08:16:58 +0000

    corrected the url

--- ubyssey/views/automate_adding_tags.py
+++ ubyssey/views/automate_adding_tags.py
@@ -14,8 +14,7 @@ def get_image_urls(request):
     for image in images:
         url = request.build_absolute_uri(base_url + image.file.name)
         image_urls.append(url)
-        # print(url)
-    get_image_tags(image_urls)
+    get_image_tags([image_urls[0],image_urls[5555]])
         
     # Pass image_urls to OpenAI API or render them in a template if needed
     return render(request, 'centennial.html', {})
@@ -54,51 +53,49 @@ def get_image_tags(image_urls):
     #     messages=messages,
     #     max_tokens=300,
     # )
+    messages = [
+        {
+            "role": "user",
+            "content": "Can you write tags and description only with no timepass information. All the pictures are mostly set in UBC where the description is for UBC students. It is all from this open source website https://ubyssey.ca/ In tags describe what happens in the picture and the medium of photography, in the description describe the image that helps in indexing the picture to search faster. For example it can be like \"tag1\", \"tag2\", \"description\". Where there should be space between each field and there should be double quotes for tags and description and, between tags and description to differentiate between them. Please no irrelevant information other than the actual tags and description"
+        },
+    ]
+    for url in image_urls:
+        messages.append({
+            "role": "user",
+            "content": url
+        },)
+
     response = client.chat.completions.create(
         model="gpt-4o",
-        messages=[
-            {
-                "role": "user",
-                "content": "Can you write tags and description only with no timepass information. In tags describe what happens in the picture and the medium of photography, in the description describe the image that helps in indexing the picture to search faster. I want it to be like \"snowfield\", \"playing\", \"People are playing in the snow\". Where there should be space between each field and there should be double quotes for tags and description and, between tags and description to differentiate between them. Please no irrelevant information other than the actual tags and description"
-            },
-            {
-                "role": "user",
-                "content": "https://ubyssey.storage.googleapis.com/media/wagtail_images/2024/04/240429_i_janmohamed_encampment.JPG"
-            }
-        ],
+        messages=messages,
         temperature=1,
-        max_tokens=256,
+        max_tokens=200,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-    # prompt = (
-    #     "Can you write tags and description only with no timepass information. "
-    #     "In tags describe what happens in the picture and the medium of photography, "
-    #     "in the description describe the image that helps in indexing the picture to search faster. "
-    #     "I want it to be like \"snowfield\" \"playing\",\"People are playing in the snow\". "
-    #     "Where there should be space between each field and there should be double quotes for tags and description and , "
-    #     "between tags and description to differentiate between them. Please no irrelevant information other than the actuals tags and description"
-    # )
-    # client = OpenAI()
-    # response = client.ChatCompletion.create(
-    #     model="gpt-4",
-    #     messages=[
-    #         {
-    #             "role": "user",
-    #             "content": f"[{prompt} Image URL: {image_urls[0]}]"
-    #         }
-    #     ],
+    choices = response.choices
+    chat_completion = choices[0]
+    content = chat_completion.message.content # Correct (this works with the Chat Completions API)
+    print(content+"                  ")
+
+    # # Assuming image_urls is a list of URLs to process similarly
+    # for url in image_urls:
+    #     messages.append({
+    #         "role": "user",
+    #         "content": url
+    #     })
+
+    # response = client.chat.completions.create(
+    #     model="gpt-4o",
+    #     messages=messages,
     #     temperature=1,
-    #     max_tokens=100,
+    #     max_tokens=200,
     #     top_p=1,
     #     frequency_penalty=0,
     #     presence_penalty=0
     # )
-    choices = response.choices
-    chat_completion = choices[0]
-    content = chat_completion.message.content # Correct (this works with the Chat Completions API)
-    print(content)
+    # print(response.choices[1].message.content)
 
 def populate_tags(tags, descriptions):
     images = UbysseyImage.objects.all()
