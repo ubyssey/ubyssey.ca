dafadd176a5cf16675c52b5b3ccde5a52fe08a61
Nishim12 <nishimsinghi@gmail.com>
2024-07-16 11:25:36 +0000

    got test cases to work

--- tests_ubyssey/tests.py
+++ tests_ubyssey/tests.py
@@ -1,3 +1,4 @@
+from asyncio import wait
 from lib2to3.pgen2 import driver
 from telnetlib import EC
 import time
@@ -164,10 +165,12 @@ class TestUbyssey(StaticLiveServerTestCase):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--headless")  # Add headless mode if running in a non-GUI environment
-        # self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
-        self.driver = webdriver.Chrome(options=chrome_options)
+        prefs = {"profile.default_content_settings.cookies": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        # self.driver = webdriver.Chrome(options=chrome_options)
         # self.driver.get('https://ubyssey.ca/')
-        self.driver.get('https://ubyssey.ca/')
+        self.driver.get('http://localhost:8000/')
         assert "The Ubyssey" in self.driver.title
         self.driver.set_window_size(1296, 688)
         self.driver.find_element(By.CSS_SELECTOR, ".middle .nav > li:nth-child(1) > a").click()
@@ -185,7 +188,14 @@ class TestUbyssey(StaticLiveServerTestCase):
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(9) > a").click()
         self.driver.find_element(By.CSS_SELECTOR, ".home-link > .light-logo").click()
         self.driver.find_element(By.CSS_SELECTOR, ".o-article:nth-child(2) > .o-article__right > .o-article__headline > a").click()
-        # self.driver.execute_script("window.scrollTo(0,444.4444885253906)")
+        html_content = self.driver.page_source
+        # output_file = "output.html"
+        # with open(output_file, "w", encoding="utf-8") as file:
+        #     file.write(html_content)
+        self.driver.get('http://localhost:8000/')
+        self.driver.execute_script("window.scrollTo(0,444.4444885253906)")
+        self.driver.find_element(By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a").click()        
+
         # assert 'Top Stories' == self.driver.find_element(By.CSS_SELECTOR, 'h1.boxText.boxText-left').text        # self.driver.find_element(By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a").click()
         # article_element = driver.find_element(By.CSS_SELECTOR, 'article.o-article.o-article--coverstory').click()
         # wait = WebDriverWait(driver, 30)  # Wait up to 10 seconds
@@ -196,12 +206,62 @@ class TestUbyssey(StaticLiveServerTestCase):
         # element.click()
 
         # self.driver.find_element(By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a").click()
+        # self.driver.get('http://localhost:8000/')
+        # self.driver.execute_script("window.scrollTo(0,444.4444885253906)")
         # self.driver.execute_script("window.scrollTo(0,1111.1112060546875)")
         # self.driver.execute_script("window.scrollTo(0,1222.2222900390625)")
         # self.driver.find_element(By.CSS_SELECTOR, ".c-infinitefeed__feed:nth-child(1) .c-homepage__section:nth-child(2) > .o-article .o-article__headline > a").click()
         # self.driver.find_element(By.CSS_SELECTOR, ".c-infinitefeed__feed:nth-child(1) .c-homepage__section:nth-child(2) .o-article:nth-child(1) > .o-article__meta a").click()
         # self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) .o-article__headline > a").click()
+        self.driver.get("http://localhost:8000/")
+        self.driver.set_window_size(1296, 688)
+        self.driver.find_element(By.CSS_SELECTOR, ".o-article:nth-child(2) > .o-article__right > .o-article__headline > a").click()
+
         element = self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(4) > a")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
-        print("Success")       
+        self.driver.get("http://localhost:8000/")
+        self.driver.set_window_size(1296, 688)
+        # wait = WebDriverWait(self.driver, 10)
+        # cache_panel_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.CachePanel")))
+        # cache_panel_link.click()
+        # wait = WebDriverWait(self.driver, 10)
+        # continue_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-button.c-button--small")))
+        # continue_button.click()
+        # Locate the news element and scroll to it
+        wait = WebDriverWait(self.driver, 10)
+        news_story = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) > .o-article .o-article__headline > a")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", news_story)
+        news_story.click()
+        
+        # Locate the second element and scroll to it
+        news_story = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-article")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", news_story)
+        news_story.click()
+        self.driver.get("http://localhost:8000/")
+        self.driver.set_window_size(1296, 688)
+        # Locate and scroll to the first news recommendation in the left sidebar then click it
+        news_first_recommendation = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) .o-article:nth-child(1) .o-article__headline")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", news_first_recommendation)
+        news_first_recommendation.click()
+        # news_first_recommendation = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) .o-article:nth-child(1) > .o-article__meta a")))
+        # self.driver.execute_script("arguments[0].scrollIntoView(true);", news_first_recommendation)
+        # news_first_recommendation.click()
+        self.driver.get("http://localhost:8000/")
+
+        # Locate and scroll to the first  recommended article element in the right sidebar then click it
+        first_recommended_article = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li:nth-child(1) .o-article__headline > a")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", first_recommended_article)
+        first_recommended_article.click()
+        self.driver.get("http://localhost:8000/")
+
+        # Locate and scroll to the author link on news article then click it
+        author_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) .o-article__byline a:nth-child(1)")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", author_link)
+        author_link.click()
+        # Locate the sun icon to switch to dark mode
+        sun_icon = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".right .sun-and-moon")))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", sun_icon)
+        sun_icon.click()
+
+        print("Success") 
