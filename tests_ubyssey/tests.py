f56153841b73164bca66265e593a3d4e838bde0b
Nishim12 <nishimsinghi@gmail.com>
2024-07-25 05:04:39 +0000

    test cases work

--- tests_ubyssey/tests.py
+++ tests_ubyssey/tests.py
@@ -196,12 +196,8 @@
 #         # article_element = driver.find_element(By.CSS_SELECTOR, 'article.o-article.o-article--coverstory').click()
 #         # wait = WebDriverWait(driver, 30)  # Wait up to 10 seconds
 #         # element = WebDriverWait(driver, 10).until(
-#         #     EC.presence_of_element_located((By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a"))
-#         # )
-#         # # Click the link within the article
-#         # element.click()
-
-#         # self.driver.find_element(By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a").click()
+#         #     EC.presence_of_element_located((By.CSS_SELECTOR, "        raise ValueError(f"Unsupported browser: {self.browser}")ck the link within the article
+#         # eleme        raise ValueError(f"Unsupported browser: {self.browser}")-coverstory .o-article__headline > a").click()
 #         # self.driver.get('http://localhost:8000/')
 #         # self.driver.execute_script("window.scrollTo(0,444.4444885253906)")
 #         # self.driver.execute_script("window.scrollTo(0,1111.1112060546875)")
@@ -316,7 +312,7 @@ class BaseTestCase(StaticLiveServerTestCase):
                 command_executor='http://selenium-firefox:4444/wd/hub',
                 options=firefox_options
             )
-        else:  # default to Edge
+        else:
             edge_options = webdriver.EdgeOptions()
             edge_options.add_argument('--headless')
             self.driver = webdriver.Remote(
@@ -324,40 +320,50 @@ class BaseTestCase(StaticLiveServerTestCase):
                 options=edge_options
             )
         self.driver.implicitly_wait(5)
-
+    
     def tearDown(self):
         self.driver.quit()
         super().tearDown()
 
-    def test_ubysseys(self):
+    def article_page_exists(self):
+        author_cards = self.driver.find_elements(By.CLASS_NAME, 'author_card')
+        self.assertGreater(len(author_cards), 0, "No author cards found on the page")
+
+    def test_navigation_menu(self):
         self.driver.get('http://host.docker.internal:8000/')
         assert "The Ubyssey" in self.driver.title
         button = self.driver.find_element(By.CSS_SELECTOR, ".c-button.c-button--small")
-
-        # Click the button
         button.click()
-
         self.driver.set_window_size(1296, 688)
         self.driver.find_element(By.CSS_SELECTOR, ".middle .nav > li:nth-child(1) > a").click()
+        assert "News" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(2) > a").click()
+        assert "Culture" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(3) > a").click()
+        assert "Features" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(4) > a").click()
+        assert "Opinion" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(5) > a").click()
+        assert "Humour" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(6) > a").click()
+        assert "Science" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(7) > a").click()
+        assert "Sports" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(8) > a").click()
+        assert "Photo" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(9) > a").click()
+        assert "Videos" in self.driver.title
         self.driver.find_element(By.CSS_SELECTOR, ".home-link > .light-logo").click()
-        self.driver.find_element(By.CSS_SELECTOR, ".o-article:nth-child(2) > .o-article__right > .o-article__headline > a").click()
+        assert "The Ubyssey" in self.driver.title
+        self.driver.find_element(By.CSS_SELECTOR, ".o-article:nth-child(2) > .o-article__right > .o-article__headline > a").click()        
+
+    def test_cover_story(self):
         self.driver.get('http://host.docker.internal:8000/')
         self.driver.execute_script("window.scrollTo(0,444.4444885253906)")
-        self.driver.delete_all_cookies()
         self.driver.find_element(By.CSS_SELECTOR, ".o-article--coverstory .o-article__headline > a").click()
-        # self.driver.get('http://host.docker.internal:8000/')
-        # self.driver.set_window_size(1296, 688)
-        # element = self.driver.find_element(By.CSS_SELECTOR, "nav > .nav > li:nth-child(4) > a")
-        # actions = ActionChains(self.driver)
-        # actions.move_to_element(element).perform()
+        self.article_page_exists()
+
+    def test_home_page_news_block(self):
         self.driver.get('http://host.docker.internal:8000/')
         self.driver.set_window_size(1296, 688)
         wait = WebDriverWait(self.driver, 10)
@@ -367,30 +373,65 @@ class BaseTestCase(StaticLiveServerTestCase):
         news_story = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-article")))
         self.driver.execute_script("arguments[0].scrollIntoView(true);", news_story)
         news_story.click()
+        self.article_page_exists()
+
+    def test_author_page(self):
         self.driver.get('http://host.docker.internal:8000/')
-        author_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) .o-article__byline a:nth-child(1)")))
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", author_link)
-        author_link.click()
-        sun_icon = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".right .sun-and-moon")))
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", sun_icon)
-        sun_icon.click()
+        self.driver.find_element(By.CSS_SELECTOR, ".c-button.c-button--small").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".o-article__meta:nth-child(2) > .o-article__byline a:nth-child(1)").click()
+        author_cards = self.driver.find_elements(By.CLASS_NAME, 'author-info')
+        self.assertGreater(len(author_cards), 0, "Author page does not exists")    
+
+    def test_dark_mode(self):
         self.driver.get('http://host.docker.internal:8000/')
+        hide_element = self.driver.find_element(By.ID, "djHideToolBarButton")
+        hide_element.click()
         self.driver.set_window_size(1296, 688)
-        news_first_recommendation = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".c-homepage__section:nth-child(1) .o-article:nth-child(1) .o-article__headline")))
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", news_first_recommendation)
-        news_first_recommendation.click()
+        script = '''
+        return getComputedStyle(document.documentElement)
+            .getPropertyValue('--background').trim();
+        '''
+        # Execute the JavaScript and get the background value
+        background_nav_value = self.driver.execute_script(script)
+        # Check if the value matches the expected RGBA value
+        assert background_nav_value == 'white', f"The background-nav color is incorrect. Found: {background_nav_value}"
+
+        self.driver.find_element(By.CSS_SELECTOR, ".right .sun-and-moon").click()
+        script = '''
+        return getComputedStyle(document.documentElement)
+            .getPropertyValue('--background').trim();
+        '''
+        # Execute the JavaScript and get the background value
+        background_nav_value = self.driver.execute_script(script)
+
+        # Check if the value matches the expected RGBA value
+        assert background_nav_value == '#031621', f"The background-nav color is incorrect. Found: {background_nav_value}"
+
+        self.driver.find_element(By.CSS_SELECTOR, ".right > #theme-toggle .sun").click()
+
+    def test_news_featured_section_first_article(self):
+        self.driver.get('http://host.docker.internal:8000/')
+        self.driver.find_element(By.CSS_SELECTOR, ".c-button.c-button--small").click()
+        self.driver.set_window_size(1296, 688)
+        self.driver.find_element(By.CSS_SELECTOR, ".c-infinitefeed__feed:nth-child(1) .c-homepage__section:nth-child(2) .o-article:nth-child(1) > .o-article__meta a").click()
+        self.article_page_exists()
+
+    def test_search_bar_news(self):
         self.driver.get("http://host.docker.internal:8000/news/")
         self.driver.find_element(By.CSS_SELECTOR, ".c-button.c-button--small").click()
         self.driver.set_window_size(1296, 688)
         self.driver.find_element(By.CSS_SELECTOR, ".o-archive__search__label").click()
         self.driver.find_element(By.ID, "c-articles-list__searchbar").send_keys("UBCO")
         self.driver.find_element(By.ID, "c-articles-list__searchbar").send_keys(Keys.ENTER)
+        articles = self.driver.find_elements(By.CSS_SELECTOR,"#feed article")
+        # Assert that there is at least one article after searching
+        assert len(articles) > 0, "No articles found in the feed section."
 
-class EdgeTestCase(BaseTestCase):
-    browser = 'edge'
+# class EdgeTestCase(BaseTestCase):
+#     browser = 'edge'
 
 class ChromeTestCase(BaseTestCase):
     browser = 'chrome'
 
-class FirefoxTestCase(BaseTestCase):
-    browser = 'firefox'
+# class FirefoxTestCase(BaseTestCase):
+#     browser = 'firefox'
