7590ff9f7e53df5f0080d4bec371512a6bc2dd8e
SamuelmdLow <samuel.m.d.low@gmail.com>
2024-09-02 07:00:01 +0000

    queer substance abuse article: fix gsap scrolltrigger resizing issue, do meta tags

--- ubyssey/static_src/src/js/queer-substance-abuse.js
+++ ubyssey/static_src/src/js/queer-substance-abuse.js
@@ -4,13 +4,16 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 console.log("hello?");
 gsap.registerPlugin(ScrollTrigger);
 
-var covers = gsap.utils.toArray('.fade-in-out');
-covers.forEach((cover) => {
-    console.log(cover);
-    gsap.to(cover, {
-        scrollTrigger: {trigger: cover, start: "start center", end: "bottom center", scrub: true,  
-            onToggle: self => {self.trigger.classList.toggle('visible');console.log("huh????");}
-        },
-        immediateRender: false,
-    });
-})
+let mm = gsap.matchMedia();
+mm.add("(min-width: 1px)", () => {
+    var covers = gsap.utils.toArray('.fade-in-out');
+    covers.forEach((cover) => {
+        console.log(cover);
+        gsap.to(cover, {
+            scrollTrigger: {trigger: cover, start: "start center", end: "bottom center", scrub: true,  
+                onToggle: self => {self.trigger.classList.toggle('visible');console.log("huh????");}
+            },
+            immediateRender: false,
+        });
+    })
+});
