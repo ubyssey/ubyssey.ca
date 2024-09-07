9f294c94afd1798b16a3fb761af2ee993899332e
SamuelmdLow <samuel.m.d.low@gmail.com>
2024-09-07 03:14:57 +0000

    Hopefully final changes to queery substance abuse article

--- ubyssey/static_src/src/js/queer-substance-abuse.js
+++ ubyssey/static_src/src/js/queer-substance-abuse.js
@@ -1,17 +1,16 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-console.log("hello?");
 gsap.registerPlugin(ScrollTrigger);
 
 let mm = gsap.matchMedia();
-mm.add("(min-width: 1px)", () => {
+mm.add("(min-width: 1px), (min-height: 1px)", () => {
     var covers = gsap.utils.toArray('.fade-in-out');
     covers.forEach((cover) => {
         console.log(cover);
         gsap.to(cover, {
             scrollTrigger: {trigger: cover, start: "start center", end: "bottom +=300", scrub: true,  
-                onToggle: self => {self.trigger.classList.toggle('visible');console.log("huh????");}
+                onToggle: self => {self.trigger.classList.toggle('visible');}
             },
             immediateRender: false,
         });
