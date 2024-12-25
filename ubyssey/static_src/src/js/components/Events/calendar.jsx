fc35b0e791713a0af1ea293e501fb26fd35956e8
Nishim12 <nishimsinghi@gmail.com>
2024-12-25 23:32:42 +0000

    added loader

--- ubyssey/static_src/src/js/components/Events/calendar.jsx
+++ ubyssey/static_src/src/js/components/Events/calendar.jsx
@@ -7,7 +7,8 @@ import {
     useNavigate,
 } from "react-router-dom";
 // import ReactDOM from 'react-dom';
-import axios from "axios";
+import Throbber from '../../../images/throbber.svg';
+import DarkmodeToggle from '../../../images/darkmode.svg';
 
 const BP_DESKTOP_SIZE = 1199;
 
@@ -251,23 +252,7 @@ export function QueryEventsCalendar() {
                                 aria-label="auto" 
                                 aria-live="polite"
                             >
-                                <svg className="sun-and-moon" aria-hidden="true" width="1.75em" height="1.75em" viewBox="0 0 24 24">
-                                    <mask className="moon" id="moon-mask">
-                                        <rect x="0" y="0" width="100%" height="100%" fill="white" />
-                                        <circle cx="24" cy="10" r="6" fill="black" />
-                                    </mask>
-                                    <circle className="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
-                                    <g className="sun-beams" stroke="currentColor">
-                                        <line x1="12" y1="1" x2="12" y2="3" />
-                                        <line x1="12" y1="21" x2="12" y2="23" />
-                                        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-                                        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-                                        <line x1="1" y1="12" x2="3" y2="12" />
-                                        <line x1="21" y1="12" x2="23" y2="12" />
-                                        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-                                        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-                                    </g>
-                                </svg>
+                                <DarkmodeToggle />
                             </button>
                         </div>
                         <h1 class="title">Events around campus</h1>
@@ -971,23 +956,7 @@ function EventInfoBox({event}) {
 function LoaderComponent({ width }) {
     return (
         <div className="loader">
-            <svg width={width} height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <style>
-                    {`
-                    .spinner_ajPY {
-                        transform-origin: center;
-                        animation: spinner_AtaB 0.75s infinite linear;
-                    }
-                    @keyframes spinner_AtaB {
-                        100% {
-                        transform: rotate(360deg);
-                        }
-                    }
-                    `}
-                </style>
-                <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/>
-                <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_ajPY"/>
-            </svg>
+            <Throbber />
         </div>
     );
 }
