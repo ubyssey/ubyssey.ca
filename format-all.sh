c8e7dff4017d22b074528ea60a931cdea3c8f041
Nishim12 <nishimsinghi@gmail.com>
2025-02-14 07:23:53 +0000

    Format all files

--- format-all.sh
+++ format-all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# format-all.sh
-# This script applies prettier to all tracked files
 
 # List all tracked files and pass them to prettier via xargs
 git ls-files -z | xargs -0 npx prettier --write
 pre-commit install
+pre-commit run --all-files
