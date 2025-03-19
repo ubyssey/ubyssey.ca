b246586b3c7ff9a03989b6e216e9c58c73d2098e
SamuelmdLow <samuel.m.d.low@gmail.com>
2025-03-19 02:35:57 +0000

    Started spoof 2025.

--- ubyssey/urls.py
+++ ubyssey/urls.py
@@ -60,6 +60,8 @@ urlpatterns += [
     # re_path(r'^magazine/(?P<year>[0-9]{4})/$', magazine.magazine, name='magazine-landing'),
     # re_path(r'^magazine/(?P<slug>[-\w]+)/$', magazine.article, name='magazine-article'),
 
+    re_path(r'humour/girlslowermainland', TemplateView.as_view(template_name='spoof/2025/landing.html')),
+
     re_path(r'^djadmin/', admin.site.urls),
 
     # re_path(r'^admin', include(admin_urls)),
