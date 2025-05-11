98c94771cd00079692a3936d1cb00d96ab8d1c72
SamuelmdLow <samuel.m.d.low@gmail.com>
2025-05-11 01:22:42 +0000

    Changed from Page chooser to specific authors chooser to be the same as choosing authors in articles (also its ordered nicely)

--- images/forms.py
+++ images/forms.py
@@ -1,16 +1,22 @@
 from wagtail.images.forms import BaseImageForm
-from wagtail.admin.widgets import AdminPageChooser
+from wagtail.admin.widgets import BaseChooser
 from wagtail.admin.staticfiles import versioned_static
 from django import forms
 
-class AdminPageChooserWithModalWorkflowScript(AdminPageChooser):
-    '''
-    The wagtail image editor does not have the modal-workflow.js script
-    AdminPageChooser assumes that this script is loaded.
-    Since it isn't, we have to include it in the scripts added by the page chooser
-    '''
+class ImagesAuthorChooser(BaseChooser):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = "authors.AuthorPage"
+        self.icon = 'user'
+        self.chooser_modal_url_name = "author_chooser:choose"
+
     @property
     def media(self):
+        '''
+        The wagtail image editor does not have the modal-workflow.js script
+        AdminPageChooser assumes that this script is loaded.
+        Since it isn't, we have to include it in the scripts added by the page chooser
+        '''
         modal_workflow_script = forms.Media(
             js=[
                 versioned_static("wagtailadmin/js/modal-workflow.js")
@@ -24,4 +30,4 @@ class UbysseyImageForm(BaseImageForm):
     '''
     class Meta(BaseImageForm.Meta):
         widgets = BaseImageForm.Meta.widgets
-        widgets["author"] = AdminPageChooserWithModalWorkflowScript(target_models=["authors.AuthorPage"])
+        widgets["author"] = ImagesAuthorChooser()
