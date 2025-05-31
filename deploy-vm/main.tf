e160138d5ec959689ff05b4038f31340d932cb38
Nishim Singhi <nishimsinghi@gmail.com>
2025-05-31 23:10:50 +0000

    Fixed script to use latest snapshot

--- deploy-vm/main.tf
+++ deploy-vm/main.tf
@@ -5,41 +5,35 @@ provider "google" {
 }
 
 data "google_compute_snapshot" "ubyssey_snapshot" {
-  name    = "ubyssey-prd-vm-1-us-west1-a-20250504090431-kiy1orqn"
-  project = "ubyssey-prd"
+  project     = "ubyssey-prd"
+  filter      = "name eq ^ubyssey-prd-vm.*"
+  most_recent = true
 }
 
-# Optional: Generate a random suffix for the disk name to ensure uniqueness
 resource "random_id" "suffix" {
   byte_length = 4
 }
+//TODO: add snapshot of the disk
 
-# 1. Create a new disk from the snapshot to be used as the boot disk
 resource "google_compute_disk" "boot_disk_from_snapshot" {
-  name  = "boot-disk-from-snapshot-${random_id.suffix.hex}" # Use a unique name, e.g., with a random suffix
-  zone  = "us-west1-a"
-  type  = "pd-standard"
-  # The size of the new disk must be at least the size of the snapshot's source disk.
-  # You can explicitly set a size if needed, otherwise it will default to the snapshot size.
-  # size = 40 # Example: explicitly setting size
-
-  # CORRECTED: Use the 'snapshot' argument to specify the source snapshot
+  name     = "boot-disk-from-snapshot-${random_id.suffix.hex}"
+  size     = 40
+  type     = "pd-balanced"
   snapshot = data.google_compute_snapshot.ubyssey_snapshot.self_link
 }
 
-
+//TODO: update name
 resource "google_compute_instance" "ubyssey_prd_vm_2" {
-  name         = "ubyssey-prd-vm-2"
-  machine_type = "e2-highcpu-8"
-  zone         = "us-west1-a" # Must be in the same zone as the boot disk
-  description  = ""
+  name                      = "ubyssey-prd-vm-2"
+  //TODO: create a variable for the name
+  machine_type              = "e2-highcpu-8"
+  zone                      = "us-west1-a"
+  description               = ""
   allow_stopping_for_update = true
 
-  # 2. Reference the newly created disk in the boot_disk block
   boot_disk {
     auto_delete = true
-    # Use the 'source' argument to attach the disk created from the snapshot
-    source = google_compute_disk.boot_disk_from_snapshot.self_link
+    source      = google_compute_disk.boot_disk_from_snapshot.self_link
   }
 
   network_interface {
@@ -57,9 +51,9 @@ resource "google_compute_instance" "ubyssey_prd_vm_2" {
   }
 
   metadata = {
+    startup-script = "sudo ufw allow 22"
     enable-osconfig = "TRUE"
   }
-
   tags = ["http-server", "https-server"]
 
   labels = {
@@ -68,5 +62,10 @@ resource "google_compute_instance" "ubyssey_prd_vm_2" {
 
   reservation_affinity {
     type = "ANY_RESERVATION"
+  }
+
+  service_account {
+    email  = "863738545301-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
