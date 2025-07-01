3ce57bddfcad3e192eebef1155c3d5a8b9d75bd0
SamuelmdLow <samuel.m.d.low@gmail.com>
2025-07-01 16:20:10 +0000

    Developing Passing special design

--- ubyssey/static_src/src/js/passing-2025.js
+++ ubyssey/static_src/src/js/passing-2025.js
@@ -3,28 +3,62 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger);
 
-let split, animation;
+let invisibility, notPossible;
 
 function setup() {
-  split && split.revert();
-  animation && animation.revert();
-  split = SplitText.create(".subtitle", {type:"chars,words,lines"});
+  invisibility = SplitText.create(".invisibility", {type:"chars"});
+  notPossible = SplitText.create(".not-possible", {type:"words"})
 }
 
 setup();
 window.addEventListener("resize", setup);
 
-let tl = gsap.from(split.chars, {
-    x: 150,
+const tl = gsap.timeline();
+
+tl.from(invisibility.chars, {
+    x: 50,
+    y: -50,
     opacity: 0,
-    duration: 0.7, 
+    duration: 1, 
     ease: "power4",
     stagger: 0.04
-})
+});
+
+tl.from(notPossible.words, {
+    y: -50,
+    opacity: 0,
+    duration: 1, 
+    ease: "expo",
+    stagger: 1
+});
+
+tl.to('.invisibility', {
+    opacity: 0.5,
+    duration: 0.25, 
+    ease: "power4",
+});
 
 ScrollTrigger.create({
     trigger: '.banner',
 	start: 'top top',
     animation: tl,
-})
+    pin: true,
+    scrub: true,
+});
+
+const footertl = gsap.timeline();
+
+footertl.from('footer', {
+    y: 200,
+    duration: 1, 
+    ease: "power4",
+});
+
+ScrollTrigger.create({
+    trigger: 'footer',
+	start: 'top bottom',
+    animation: footertl,
+    scrub: true,
+});
