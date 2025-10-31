24e2a890dc35b1f137c5332bb94939a3308e40ff
Nishim Singhi <nishimsinghi@gmail.com>
2025-10-31 05:24:41 +0000

    added to not minify react and javascript online on browser tools

--- ubyssey/static_src/webpack.dev.js
+++ ubyssey/static_src/webpack.dev.js
@@ -5,6 +5,28 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', {
+                runtime: 'automatic',
+                development: true,
+                importSource: '@react'
+              }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.traceDeprecation': true
