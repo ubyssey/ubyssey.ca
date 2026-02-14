38450d4088f5c957424f905da61f57399ccfe681
Nishim Singhi <nishimsinghi@gmail.com>
2026-02-14 21:05:55 +0000

    removed the health check

--- config/settings/base.py
+++ config/settings/base.py
@@ -129,10 +129,6 @@ INSTALLED_APPS = [
     'wagtailmenus',
     'wagtailcache',
     'wagtail_color_panel',
-    'health_check',
-    'health_check.db',
-    'health_check.cache',
-    'health_check.storage',
 ]
 
 if DEBUG:
