3d2310c261877dda3edca859cbf991cb654cc45e
Deacon Sowerby <deaconsowerby8@gmail.com>
2026-04-29 22:24:13 +0000

    Cleaned up demo

--- new_cms/urls.py
+++ new_cms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'new_cms'
+app_name = "new_cms"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('page/<int:page_id>', views.page_edit, name='page_edit')
+    path("", views.index, name="index"),
+    path("page/<int:page_id>", views.manuscript_editor, name="manuscript_editor"),
     # I'll add more later
 ]
